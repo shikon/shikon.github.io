34af8580ca8cc62989f0f36a93be1394a7d58a03
User Ryuuta <lan@shiko.nl>
2020-12-27 15:58:26 +0100

    test

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Post: Standard",
+        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["Post Formats"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/post%20formats/2010/01/07/post-standard.html",
+        "teaser": null
+      },{
+        "title": "About",
+        "excerpt":"This is the base Jekyll theme. You can find out more info about customizing your Jekyll theme, as well as basic Jekyll usage documentation at jekyllrb.com You can find the source code for Minima at GitHub: jekyll / minima You can find the source code for Jekyll at GitHub: jekyll...","categories": [],
+        "tags": [],
+        "url": "/about/",
+        "teaser": null
+      },{
+        "title": "Index",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/2020/12/27/index.html",
+        "teaser": null
+      }]
