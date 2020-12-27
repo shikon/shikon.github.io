e6383a5caece7e871b65430123b31f41a9db6849
User Ryuuta <lan@shiko.nl>
2020-12-27 22:47:06 +0100

    my first post!

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Hello world!",
-        "excerpt":"The quick brown fox jumped over  the lazy dog… This is my first test post.  ","categories": ["First steps"],
-        "tags": ["Hello world!","first time","standard"],
-        "url": "http://0.0.0.0:4000/first%20steps/hello-world/",
-        "teaser": null
-      },]
