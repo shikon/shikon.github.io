9109b0458ec8ca0c972fa1b9087fb1743e6b8e96
User Ryuuta <lan@shiko.nl>
2021-01-18 01:25:13 +0100

    new post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,24 @@ var store = [{
         "title": "Hello world!",
         "excerpt":"The quick brown fox jumped over  the lazy dog… This is my first test post.  ","categories": ["First steps"],
         "tags": ["Hello world!","first time","standard"],
-        "url": "http://0.0.0.0:4000/first%20steps/hello-world/",
+        "url": "/first%20steps/2020/12/27/hello-world.html",
         "teaser": null
-      },]
+      },{
+        "title": "Printing O-rings for dad",
+        "excerpt":"Backstory Today dad asked me if I could buy an O-ring as he needed one. The situation is as follows, the water drainage has a cup (diameter 9 cm) which seals the sewer air from the environment. It broke so it started to smell, and we all know the sewers...","categories": ["3D-printing"],
+        "tags": ["first time","standard"],
+        "url": "/3d-printing/2020/12/27/printing-orings.html",
+        "teaser": null
+      },{
+        "title": "Drawing O-rings for dad aka printing O-rings for dad part 2",
+        "excerpt":"Practicalities… While waiting for my Sainsmart 3D TPU filament to arrive, I still needed some other parts before I could start the printing. That is a CAD drawing of such O-ring. I thought I would finish this in a couple of minutes, I mean how hard could it be? I...","categories": ["3D-printing"],
+        "tags": ["first time","fusion360","CAD tool"],
+        "url": "/3d-printing/2020/12/28/drawing-orings.html",
+        "teaser": null
+      },{
+        "title": "Designing a Nixie HV supply part 1",
+        "excerpt":"Backstory Well basically the backstory is the introduction but shht :). Recently I designed a small PCB with an analog switch for my internship project. This made me remember, I still have to finish my PCB I was doing for my nixie tube clock. Don’t know what those are? Well,...","categories": ["electronics"],
+        "tags": ["PCB","CAD","design","project"],
+        "url": "/electronics/2021/01/02/a-pcb-design.html",
+        "teaser": null
+      }]
