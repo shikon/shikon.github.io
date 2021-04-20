49fc950157acd7221913dca1f48651671cf59863
Charlie Root <taruhilan@gmail.com>
2021-04-20 15:12:46 +0200

    Built `lazy update with script`

--- update.sh
+++ update.sh
@@ -2,3 +2,6 @@ git add .
 git commit -m 'lazy update with script'
 git push
 chown -R Ryuuta:Boss /mnt/myblog/shikon.github.io
+
+setenv ALGOLIA_API_KEY 033337088f8bf0e6298ea3df3e165107 
+bundle exec jekyll algolia
