a6874fa43f024fb1f014b21f5c12a2cbe4e75fc6
Charlie Root <taruhilan@gmail.com>
2021-10-12 22:36:47 +0200

    Built `lazy update with script`

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,54 +2,54 @@ var store = [{
         "title": "Hello world!",
         "excerpt":"The quick brown fox jumped over  the lazy dog… This is my first test post.  ","categories": ["electronics"],
         "tags": [],
-        "url": "/electronics/hello-world/",
+        "url": "/sad/electronics/hello-world/",
         "teaser": null
       },{
         "title": "Printing O-rings for dad",
         "excerpt":"Backstory Today dad asked me if I could buy an O-ring as he needed one. The situation is as follows, the water drainage has a cup (diameter 9 cm) which seals the sewer air from the environment. It broke so it started to smell, and we all know the sewers...","categories": ["3D-printing"],
         "tags": ["first time","standard"],
-        "url": "/3d-printing/printing-orings/",
+        "url": "/sad/3d-printing/printing-orings/",
         "teaser": null
       },{
         "title": "Drawing O-rings for dad aka printing O-rings for dad part 2",
         "excerpt":"Practicalities… While waiting for my Sainsmart 3D TPU filament to arrive, I still needed some other parts before I could start the printing. That is a CAD drawing of such O-ring. I thought I would finish this in a couple of minutes, I mean how hard could it be? I...","categories": ["3D-printing"],
         "tags": ["first time","fusion360","CAD tool"],
-        "url": "/3d-printing/drawing-orings/",
+        "url": "/sad/3d-printing/drawing-orings/",
         "teaser": null
       },{
         "title": "Designing a Nixie HV supply part 1",
         "excerpt":"Backstory Well basically the backstory is the introduction but shht :). Recently I designed a small PCB with an analog switch for my internship project. This made me remember, I still have to finish my PCB I was doing for my nixie tube clock. Don’t know what those are? Well,...","categories": ["electronics"],
         "tags": ["PCB","CAD","design","project"],
-        "url": "/electronics/a-pcb-design/",
+        "url": "/sad/electronics/a-pcb-design/",
         "teaser": null
       },{
         "title": "Automating jekyll build and github hosting",
         "excerpt":"Backstory As people might have noticed this website runs on jekyll, which is code that creates static websites. Great for my simple blogging use. The easiest way for me to get this nice skin working on github pages was to use their remote theme function. However, letting github compile the...","categories": ["programming"],
         "tags": ["automating","server","design"],
-        "url": "/programming/automating_jekyll/",
+        "url": "/sad/programming/automating_jekyll/",
         "teaser": null
       },{
         "title": "Creating a custom PCB business card with EAGLE",
         "excerpt":"An idea I have looked into making my own business cards multiple times. They would be useful to carry with you just in case. However I never came up with something unique that would be nice and presentable on a piece of nice luxury paper. However, while browsing for ideas,...","categories": ["electronics"],
         "tags": ["PCB","CAD","design"],
-        "url": "/electronics/designing_business_card/",
+        "url": "/sad/electronics/designing_business_card/",
         "teaser": null
       },{
         "title": "Creating a simple and cheap fan controller",
         "excerpt":"From the need to the idea to the solution After upgrading my computer with the new Ryzen 5800x series, the new motherboard did not include a header to connect my water temperature sensor. This lead the control of my fans to be limited to the temperature sensor inside the processor....","categories": ["electronics"],
         "tags": ["PCB","design"],
-        "url": "/electronics/pwm-fan-controller/",
+        "url": "/sad/electronics/pwm-fan-controller/",
         "teaser": null
       },{
         "title": "Designing a Nixie HV supply part 2",
         "excerpt":"The 160 V Nixie power supply design WORKS Let’s firstly state the design you will read works without issue. The high voltage nixie power supply has been working in my nixie tube clock (this will be a post later) for about half a year. No issues, as expected from the...","categories": ["electronics"],
         "tags": ["PCB","design"],
-        "url": "/electronics/boost-design/",
+        "url": "/sad/electronics/boost-design/",
         "teaser": null
       },{
         "title": "Creating a simple and cheap fan controller",
         "excerpt":"A smart home with issues Recently, I had the issue where my ZigBee network would die every few days. After a lot of debugging and even re-pairing of all my devices, I came to the conclusion my ZigBee coordinator is nearing the end of its life. Strangely enough, if you...","categories": ["programming"],
         "tags": ["Smart Home","ZigBee"],
-        "url": "/programming/espeasy-restart/",
+        "url": "/sad/programming/espeasy-restart/",
         "teaser": null
       },]
